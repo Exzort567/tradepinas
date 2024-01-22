956c7e8919291a40380587d0f13852bff9ba9bb8
Exzort <quibelkenneth@gmail.com>
2024-01-22 19:03:53 +0700

    Add post and comments

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const mongoose=require('mongoose')
 const dotenv=require('dotenv')
 const authRoute=require('./routes/auth')
 const userRoute=require('./routes/users')
+const postRoute=require('./routes/posts')
+const commentRoute=require('./routes/comments')
 
 mongoose.set('strictQuery', false);
 
@@ -22,6 +24,7 @@ dotenv.config()
 app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
+app.use("/api/posts", postRoute)
 app.listen(5000,() => {
     connectDB()
     console.log("app is running on port 5000")
---
+++ backend/routes/posts.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const router = express.Router()
+const User = require("../models/User")
+const bcrypt = require('bcrypt')
+const Post = require("../models/Post")
+const Comment = require("../models/Comment")
+
+
+//CREATE
+router.post("/create", async (req,res) => {
+    try {
+        const newPost = new Post(req.body)
+        const savedPost = await newPost.save()
+        res.status(200).json(savedPost)
+    } catch (err) {
+        res.status(200).json(err)
+    }
+})
+// Update
+router.put("/:id", async (req, res) => {
+    try {
+        
+        const updatedUser =  await Post.findByIdAndUpdate(req.params.id,{$set:req.body}, {new:true})
+        res.status(200).json(updatedUser)
+    } 
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+    try {
+        await Post.findByIdAndDelete(req.params.id)
+
+        res.status(200).json("Post has been deleted!")
+    } 
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// GET POST
+router.get("/", async (req, res) => {
+    try {
+        const posts = await Post.find()
+
+        res.status(200).json(posts)
+    } 
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// GET USER POST
+router.get("/user/:userId", async (req, res) => {
+    try {
+        const posts = await Post.find({userId:req.params.userId})
+
+        res.status(200).json(posts)
+    } 
+    catch (err) {
+        res.status(500).json(err)
+    }
+})
+ 
+module.exports=router
