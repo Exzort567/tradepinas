442dfed1d698b7abf5dedb2c1c740d288938866a
Exzort <quibelkenneth@gmail.com>
2024-02-09 11:33:28 +0700

    modify auth

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,7 @@ app.use(express.json())
 app.use("/images",express.static(path.join(__dirname, "/images")))
 app.use(cors({origin:"http://localhost:5173", credentials:true}))
 app.use(cookieParser())
+app.use("/api/auth", authRoute);
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/posts", postRoute)
@@ -50,9 +51,7 @@ app.post("/api/upload", upload.single("file"),(req,res)=>{
     // console.log(req.body)
     res.status(200).json("Image has been uploaded successfully")
 })
-app.get('/', (req, res) => {
-    res.send('Welcome to your backend server');
-  });
+
 app.listen(port,() => {
     connectDB()
     console.log("app is running on port 5000")
